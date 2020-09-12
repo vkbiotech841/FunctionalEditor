267f5f8b09cf14d82396d89ae73abb45f22cf873
VIKRAM KUMAR <vkbiotech841@gmail.com>
2020-09-12 19:18:45 +0530

    basic functionality added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import { CkeditorComponent } from './ckeditor/ckeditor.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CkeditorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CKEditorModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/ckeditor/ckeditor.component.ts
@@ -0,0 +1,75 @@
+import { Component, OnInit } from '@angular/core';
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+// import * as ClassicEditor from '../../assets/js/ck-editor-math-type/ckeditor.js';
+
+
+
+@Component({
+  selector: 'app-ckeditor',
+  templateUrl: './ckeditor.component.html',
+  styleUrls: ['./ckeditor.component.scss']
+})
+export class CkeditorComponent implements OnInit {
+
+  public Editor = ClassicEditor;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public config = {
+    placeholder: 'Type the content here!',
+    toolbar: [
+      '|',
+      'bold',
+      'italic',
+      'superscript',
+      '|',
+      'link',
+      'blockQuote',
+      '|',
+      'bulletedList',
+      'numberedList',
+      'codeBlock',
+      'undo',
+      'redo',
+      'MathType',
+      'subscript',
+      'heading',
+    ]
+
+  }
+
+
+
+  EditorContent: any = "";
+  editorContentStatus: boolean = false;
+  displayContent = "";
+
+  editorContentStore: any[] = [];
+
+
+
+  showEditorContent() {
+    this.editorContentStatus = true;
+    this.displayContent = this.EditorContent;
+    this.editorContentStore.push(this.displayContent);
+    this.EditorContent = "";
+  };
+
+
+  clearEditorContent() {
+    this.EditorContent = ""
+  };
+
+  clearOutPut() {
+    this.displayContent = ""
+    this.editorContentStore = [];
+  };
+
+  removeSingleItem(index) {
+    this.editorContentStore.splice(index, 1)
+  };
+
+}
