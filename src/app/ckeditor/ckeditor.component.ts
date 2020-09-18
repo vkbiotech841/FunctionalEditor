1fcf3a5ea94ccea0b7798ad3b7d3edcb8ef69a09
VIKRAM KUMAR <vkbiotech841@gmail.com>
2020-09-18 18:11:41 +0530

    image upload, image download as image or pdf

--- src/app/ckeditor/ckeditor.component.ts
+++ src/app/ckeditor/ckeditor.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import html2canvas from 'html2canvas';
+import { jsPDF } from "jspdf";
+
+
+
 
 
 
@@ -15,45 +20,54 @@ export class CkeditorComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
   }
 
-  public config = {
+  public ckEditorconfiguration = {
     placeholder: 'Type the content here!',
     toolbar: [
-      '|',
       'bold',
       'italic',
+      'underline',
+      '|',
       'superscript',
       'subscript',
+      '|',
+      'alignment',
+      '|',
+      'bulletedList',
+      'numberedList',
       '|',
       'link',
       'blockQuote',
       '|',
-      'bulletedList',
-      'numberedList',
       'undo',
       'redo',
+      '|',
+      'specialCharacters',
       'MathType',
       'ChemType',
-      'heading',
-      'imageUpload',
+      '|',
       'fontFamily',
-      'codeBlock',
       'fontSize',
-      'fontFamily',
       'fontColor',
-      'fontBackgroundColor'
+      'fontBackgroundColor',
+      '|',
+      'imageUpload',
+      'insertTable',
+      'heading',
+      'codeBlock',
+
 
     ]
 
-  }
+  };
 
 
 
   EditorContent: any = "";
   editorContentStatus: boolean = false;
   displayContent = "";
-
   editorContentStore: any[] = [];
 
 
@@ -81,4 +95,126 @@ export class CkeditorComponent implements OnInit {
     this.editorContentStore.splice(index, 1)
   };
 
+
+  // HTML FILE UPLOAD OR IMAGE UPLOAD:
+  images: any[] = [];
+
+  uploadImageAsUrl(event: any) {
+    this.images = [];
+    let files = event.target.files;
+    if (files) {
+      for (let file of files) {
+        let reader = new FileReader();
+        reader.onload = (e) => {
+          this.images.push(reader.result);
+          console.log("images", this.images);
+        }
+        reader.readAsDataURL(file); // Images are saved as DataURL.
+      }
+    }
+  };
+
+
+  // TAKE A SCREEN SHOT: HTML to Image using html2canvas package
+  fileName: string = "";
+
+
+  exporthtmlToImage() {
+    // Assigning File name
+    this.assigningFileName(this.fileName);
+    let element = document.getElementById("editorDisplayContent");
+    html2canvas(element)
+      .then(canvas => {
+        document.getElementById("screenShot").append(canvas);
+        let imgData = canvas.toDataURL('image/png');
+        // Downloading image
+        this.downloadImage(canvas.toDataURL(), `${this.fileName}.png`);
+        console.log("htmlToCanvas", canvas);
+        console.log("base64Img", imgData);
+      });
+  };
+
+
+  downloadImage(uri, filename) {
+    let link = document.createElement('a');
+    if (typeof link.download === 'string') {
+      link.href = uri;
+      link.download = filename;
+      //Firefox requires the link to be in the body
+      document.body.appendChild(link);
+      //simulate click
+      link.click();
+      //remove the link when done
+      document.body.removeChild(link);
+    } else {
+      window.open(uri);
+    }
+  };
+
+  assigningFileName(fileName) {
+    if (this.fileName) {
+      this.fileName = this.fileName;
+    } else {
+      this.fileName = "Untitled"
+    }
+  };
+
+
+  // CREATE A PDF FILE: HTML to pdf using html2canvas package
+
+  exportHtmlToPDF() {
+    this.assigningFileName(this.fileName);
+    let element = document.getElementById("editorDisplayContent");
+    html2canvas(element)
+      .then(canvas => {
+        console.log("exportAsPdf", canvas);
+        let imgData = canvas.toDataURL('image/png');
+        let doc = new jsPDF();
+        let imgWidth = 208;
+        let imgHeight = canvas.height * imgWidth / canvas.width;
+        // addImage takes, y axis, x axis, image width and image height
+        doc.addImage(imgData, 0, 0, imgWidth, imgHeight);
+        doc.save(`${this.fileName}.pdf`);
+        this.fileName = "";
+        console.log("imageDataBase64", imgData);
+      });
+  };
+
+
+  onReady(eventData) {
+    eventData.plugins.get('FileRepository').createUploadAdapter = function (loader) {
+      console.log('loader : ', loader)
+      console.log(btoa(loader.file));
+      return new UploadAdapter(loader);
+    };
+  };
+
+
+
 }
+
+///////////// END OF THE CLASS///////////////////////////////////
+
+
+// CKEDITOR FILE UPLOADER FOR IMAGE
+export class UploadAdapter {
+
+  private loader;
+
+  constructor(loader) {
+    this.loader = loader;
+  }
+
+  upload() {
+    return this.loader.file
+      .then(file => new Promise((resolve, reject) => {
+        var myReader = new FileReader();
+        myReader.onloadend = (e) => {
+          resolve({ default: myReader.result });
+        }
+
+        myReader.readAsDataURL(file);
+      }));
+  };
+};
+/////////////////////////////////////////////////
